f28b81e4b863aebeab5bcbee25ea72bad8c202e4
Adam Friedman <adam.friedman5@gmail.com>
2021-12-24 13:21:30 -0800

    Add HomeCenterContent to Home page

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,8 +1,59 @@
 import { Box, Typography } from "@material-ui/core";
 import Navbar from "../Navbar/Navbar";
 import { SwingingFrame } from "../SwingingFrame/SwingingFrame";
+import HomeCenterContent from "../HomeCenterContent/HomeCenterContent";
+import { makeStyles } from "@material-ui/core/styles";
+import suit from "../../Images/suit.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  "@keyframes jump": {
+    "0%": {
+      transform: "translateY(0px) rotate(0deg)",
+    },
+    "40%": {
+      transform: "translateY(0px) rotate(0deg)",
+    },
+    "50%": {
+      transform: "translateY(-100px) rotate(180deg)",
+    },
+    "60%": {
+      transform: "translateY(0px) rotate(360deg)",
+    },
+    "100%": {
+      transform: "translateY(0px) rotate(360deg)",
+    },
+  },
+  "@keyframes swirl-in": {
+    "0%": {
+      WebkitTransform: "rotate(-540deg) scale(0)",
+      transform: "rotate(-540deg) scale(0)",
+      opacity: 0,
+    },
+    "100%:": {
+      WebkitTransform: "rotate(0) scale(1)",
+      transform: "rotate(0) scale(1)",
+      opacity: 1,
+    },
+  },
+  homeWrapper: {
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    backgroundImage: `url(${suit})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100vh",
+    [theme.breakpoints.down("sm")]: {
+      backgroundPosition: "center",
+    },
+    [theme.breakpoints.down("xs")]: {
+      backgroundPosition: "center",
+      overflowY: "scroll",
+    },
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
   return (
     <>
       <Navbar />
@@ -12,11 +63,13 @@ export default function Home() {
         height="100vh"
         paddingTop="10vh"
         border="1px solid red"
+        className={classes.homeWrapper}
       >
         <Typography variant="h1" component="h1">
           Home
         </Typography>
         <SwingingFrame />
+        <HomeCenterContent />
       </Box>
       <Box
         id="story"
